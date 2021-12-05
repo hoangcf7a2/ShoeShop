36be7328605676a5e08db1fa47776faeca50e45d
hoangcf7a2 <hoangcf7a2@gmail.com>
2021-12-05 21:46:54 +0700

    add auth

--- routes/admin.js
+++ routes/admin.js
@@ -25,49 +25,49 @@ const isAuth = require('../middleware/is-auth');
  *         '500':
  *          description: Error
  */
-router.post('/user',adminController.createUser);
-router.get('/users',adminController.getUsers);
-router.get('/user/:userId',adminController.getUser);
-router.delete('/user/:userId',adminController.deleteUser);
-router.put('/user/:userId',adminController.updateUser);
+router.post('/user',isAuth,adminController.createUser);
+router.get('/users',isAuth,adminController.getUsers);
+router.get('/user/:userId',isAuth,adminController.getUser);
+router.delete('/user/:userId',isAuth,adminController.deleteUser);
+router.put('/user/:userId',isAuth,adminController.updateUser);
 
 //Size
-router.post('/size',adminController.createSize);
-router.get('/sizes',adminController.getSizes);
-router.get('/size/:sizeId',adminController.getSize);
-router.delete('/size/:sizeId',adminController.deleteSize);
-router.put('/size/:sizeId',adminController.updateSize);
+router.post('/size',isAuth,dminController.createSize);
+router.get('/sizes',isAuth,adminController.getSizes);
+router.get('/size/:sizeId',isAuth,adminController.getSize);
+router.delete('/size/:sizeId',isAuth,adminController.deleteSize);
+router.put('/size/:sizeId',isAuth,adminController.updateSize);
 
 //Color
-router.post('/color',adminController.createColor);
-router.get('/colors',adminController.getColors);
-router.get('/color/:colorId',adminController.getColor);
-router.put('/color/:colorId',adminController.updateColor);
-router.delete('/color/:colorId',adminController.deleteColor);
+router.post('/color',isAuth,adminController.createColor);
+router.get('/colors',isAuth,adminController.getColors);
+router.get('/color/:colorId',isAuth,adminController.getColor);
+router.put('/color/:colorId',isAuth,adminController.updateColor);
+router.delete('/color/:colorId',isAuth,adminController.deleteColor);
 
 //Brand
-router.post('/brand',adminController.createBrand);
-router.get('/brands',adminController.getBrands);
-router.get('/brand/:brandId',adminController.getBrand);
-router.put('/brand/:brandId',adminController.updateBrand);
-router.delete('/brand/:brandId',adminController.deleteBrand);
+router.post('/brand',isAuth,adminController.createBrand);
+router.get('/brands',isAuth,adminController.getBrands);
+router.get('/brand/:brandId',isAuth,adminController.getBrand);
+router.put('/brand/:brandId',isAuth,adminController.updateBrand);
+router.delete('/brand/:brandId',isAuth,adminController.deleteBrand);
 
 // Category
-router.post('/category',adminController.createCategory);
-router.get('/categories',adminController.getCategories);
-router.get('/category/:categoryId',adminController.getCategory);
-router.put('/category/:categoryId',adminController.updateCategory);
-router.delete('/category/:categoryId',adminController.deleteCategory);
+router.post('/category',isAuth,adminController.createCategory);
+router.get('/categories',isAuth,adminController.getCategories);
+router.get('/category/:categoryId',isAuth,adminController.getCategory);
+router.put('/category/:categoryId',isAuth,adminController.updateCategory);
+router.delete('/category/:categoryId',isAuth,adminController.deleteCategory);
 
 //Product
-router.post('/product',adminController.createProduct);
-router.put('/product/:productId',adminController.updateProduct);
+router.post('/product',isAuth,adminController.createProduct);
+router.put('/product/:productId',isAuth,adminController.updateProduct);
 router.get('/products',adminController.getProducts);
 router.get('/product/:productId',adminController.getProduct);
-router.delete('/product/:productId',adminController.deleteProduct);
+router.delete('/product/:productId',isAuth,adminController.deleteProduct);
 
 // Order
-router.put('/order/:orderId',adminController.updateOrder);
-router.get('/orders',adminController.getOrders);
-router.get('/order/:orderId',adminController.getOrder);
+router.put('/order/:orderId',isAuth,adminController.updateOrder);
+router.get('/orders',isAuth,adminController.getOrders);
+router.get('/order/:orderId',isAuth,adminController.getOrder);
 module.exports = router;
